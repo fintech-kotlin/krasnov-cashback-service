8103ad303f2598833c1d10c4a9e6bc52de1b9432
Vladimir Krasnov <nanoboot@yandex.ru>
2020-03-03 23:32:49 +0300

    PR fixes

--- src/main/kotlin/ru/tinkoff/fintech/service/cashback/CashbackCalculatorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/cashback/CashbackCalculatorImpl.kt
@@ -26,16 +26,16 @@ class CashbackCalculatorImpl : CashbackCalculator {
             cashBackSum += 6.66
         }
 
-        when {
+        cashBackSum += when {
             LOYALTY_PROGRAM_BLACK == transactionInfo.loyaltyProgramName ->
-                cashBackSum = calcBlackProgramCashBack(transactionInfo, cashBackSum)
+                calcBlackProgramCashBack(transactionInfo)
             LOYALTY_PROGRAM_ALL == transactionInfo.loyaltyProgramName
                     && MCC_SOFTWARE == transactionInfo.mccCode
                     && isPalindrome(transactionInfo.transactionSum) ->
-                cashBackSum = calcAllProgramWithSoftwareMccAndPalindromeCheckCashBack(transactionInfo, cashBackSum)
+                calcAllProgramWithSoftwareMccAndPalindromeCheckCashBack(transactionInfo)
             LOYALTY_PROGRAM_BEER == transactionInfo.loyaltyProgramName && MCC_BEER == transactionInfo.mccCode ->
-                cashBackSum = calcBeerProgramWithBeerMccCashBack(transactionInfo, cashBackSum)
-
+                calcBeerProgramWithBeerMccCashBack(transactionInfo)
+            else -> 0.0
         }
 
         if (transactionInfo.cashbackTotalValue + cashBackSum > MAX_CASH_BACK)
@@ -44,33 +44,29 @@ class CashbackCalculatorImpl : CashbackCalculator {
         return round(cashBackSum * 100) / 100
     }
 
-    fun calcBlackProgramCashBack(transactionInfo: TransactionInfo, cashBackSum: Double): Double {
-        return cashBackSum + transactionInfo.transactionSum * 0.01
+    fun calcBlackProgramCashBack(transactionInfo: TransactionInfo): Double {
+        return transactionInfo.transactionSum * 0.01
     }
 
-    fun calcAllProgramWithSoftwareMccAndPalindromeCheckCashBack(
-        transactionInfo: TransactionInfo,
-        cashBackSum: Double
-    ): Double {
-        return cashBackSum + transactionInfo.transactionSum *
+    fun calcAllProgramWithSoftwareMccAndPalindromeCheckCashBack(transactionInfo: TransactionInfo): Double {
+        return transactionInfo.transactionSum *
                 ArithmeticUtils.lcm(transactionInfo.firstName.length, transactionInfo.lastName.length) / 1000.0 / 100
     }
 
-    fun calcBeerProgramWithBeerMccCashBack(transactionInfo: TransactionInfo, cashBackSum: Double): Double {
-        var cashBack = 0.0
-        when {
+    fun calcBeerProgramWithBeerMccCashBack(transactionInfo: TransactionInfo): Double {
+        return when {
             transactionInfo.firstName.equals("Олег", true)
                     && transactionInfo.lastName.equals("Олегов", true) ->
-                cashBack = transactionInfo.transactionSum * 0.1
+                transactionInfo.transactionSum * 0.1
             transactionInfo.firstName.equals("Олег", true) ->
-                cashBack = transactionInfo.transactionSum * 0.07
+                transactionInfo.transactionSum * 0.07
             getMonthNameByCurrentWithAmendment()[0].equals(transactionInfo.firstName[0], true) ->
-                cashBack = transactionInfo.transactionSum * 0.05
+                transactionInfo.transactionSum * 0.05
             getMonthNameByCurrentWithAmendment(-1)[0].equals(transactionInfo.firstName[0], true)
                     || getMonthNameByCurrentWithAmendment(1)[0].equals(transactionInfo.firstName[0], true) ->
-                cashBack = transactionInfo.transactionSum * 0.03
+                transactionInfo.transactionSum * 0.03
+            else -> 0.0
         }
-        return cashBackSum + cashBack
     }
 
 
